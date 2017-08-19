fe327602bcb3222a773961a3a2220036c6f9f690
David Melo <david.melo@utp.edu.co>
2017-08-19 17:10:24 -0500

    Actividad del lunes en sala 14 de agosto 17

---
+++ src/estructurasdedatos/utiles/nodos/Persona.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estructurasdedatos.utiles.nodos;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Persona {
+    private String nombre;
+    private String telefono;
+    private String cedula;
+  
+    public void crearPersona(String nombre,String telefono,String cedula) {
+            System.out.println("creando persona");
+        
+    }
+
+    public void setCedula(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setNombre(String juan_Andres_Perez) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public boolean getNombre() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
