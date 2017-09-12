0f58e4561747fd85f11c1461c5013268e53d01b7
David Melo <david.melo@utp.edu.co>
2017-09-12 02:11:25 -0500

    Subiendo codigo de Matriz

--- src/estructurasdedatos/cap2/arreglos/Matriz.java
+++ src/estructurasdedatos/cap2/arreglos/Matriz.java
@@ -11,4 +11,65 @@ package estructurasdedatos.cap2.arreglos;
  */
 public class Matriz {
     
+    int tFilas =3;
+    int tColumnas = 3;
+    int [][] matriz;
+
+    public Matriz(int nFilas, int nColumnas) {
+        this.tColumnas = nColumnas;  
+        this.tFilas = nFilas;
+        this.matriz = new int [nFilas][nColumnas];
+    }
+    // Se utiliza la metodologia CRUD
+    //CRUD: Insertar
+    public void insertar (int Fila, int Columna, int dato){
+            
+        if (tFilas< Fila || tColumnas < Columna){
+                
+            System.out.println("ERROR: Nuemro no correcto");
+        
+        }else {
+            this.matriz[Fila][Columna]= dato;
+        }
+    }   //CRUD: Listar
+    public void listar (){
+            
+        for (int i = 0; i<tFilas; i++){
+            for (int j = 0;j<=tColumnas;j++){
+                System.out.println(this.matriz[i][j]);
+            }
+        }
+    } //CRUD: Buscar
+    public int buscar (int Fila,int Columna){
+        
+        if (tFilas<Fila || tColumnas<Columna || Fila < 0 || Columna < 0){
+            System.out.println("ERROR: Dato no es correcto");
+        }else{
+            return this.matriz[Fila][Columna];
+        }
+        return 0;
+    } //CRUD: Actualizar
+    public int actualizar (int dato, int Fila,int Columna){
+        
+        if (tFilas<Fila || tColumnas<Columna || Fila < 0 || Columna < 0){
+            
+            System.out.println("ERROR: Dato no es correcto");
+        }else{
+            this.matriz[Fila][Columna]= dato;
+        }
+        return 0;
+    } //CRUD: Eliminar
+    public void eliminar (int Fila,int Columna){
+        
+        if (Fila>tFilas || Columna> tColumnas || Fila<0 || Columna<0){
+                   
+                    System.out.println("ERROR: Dato no es correcto");
+        }else{
+            this.matriz[Fila][Columna]= 0;
+            }
+    }
 }
+
+
+            
+
