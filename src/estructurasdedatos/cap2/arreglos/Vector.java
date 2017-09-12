b45436d55b1278ac6050cef379c43c5c018a0149
David Melo <david.melo@utp.edu.co>
2017-09-12 02:11:42 -0500

    Subiendo codigo de Vector

--- src/estructurasdedatos/cap2/arreglos/Vector.java
+++ src/estructurasdedatos/cap2/arreglos/Vector.java
@@ -10,5 +10,71 @@ package estructurasdedatos.cap2.arreglos;
  * @author tusk
  */
 public class Vector {
+    int tFila;
+    int []vector;
+
     
+    public Vector(int nFila){
+        this.tFila = nFila;
+        this.vector= new int[nFila];
+        
+    }   // Se utiliza la metodologia CRUD 
+    public void insertar (int Fila, int dato){ //CRUD: Insertar
+        if (Fila>tFila || Fila<0){
+            System.out.println("ERROR: Datos incorrectos");
+        }else{
+            this.vector[Fila]=dato;
+        }
+    }
+    public void listar (){ //CRUD: Listar
+        for (int i = 0; i<=tFila; i++){
+            System.out.println(this.vector[i]);
+        }
+    }
+    
+    public int actualizar (int Fila, int dato){ //CRUD: Actualizar
+        if(tFila<Fila || Fila<0){
+            System.out.println("ERROR: No se puede realizar operacion, dato incorrecto");
+        }else{
+         this.vector[Fila]=dato;
+        }
+        return 0;
+    }
+    public void eliminar (int Fila){ //CRUD: Eliminar
+        if (tFila<Fila || Fila<0){
+            System.out.println("ERROR: Valor no valido");
+        }else{
+            this.vector[Fila]=0;
+    }
+}                           //CRUD: Ordenar
+    public void ordenar (){ //Se utiliza el metodo de ordenamiento Burbuja
+        for(int i = 0; i<tFila; i++){
+            for(int j = i++; j<tFila; j++){
+                if(vector[i]>vector[j]){
+                    int temporal = vector [i];
+                    this.vector[i] = this.vector[j];
+                }
+            }
+        }
+    }
+    public void buscar (int dato){ //CRUD: Buscar
+        if (dato<=(tFila/2)){
+            for(int i = 0; i<=tFila/2;i++){
+                if(this.vector[i]==dato){
+                    System.out.println("La casilla donde se encuentra el numero es: "+i);
+                }else{
+                    System.out.println("ERROR: Dato incorrecto");
+                }
+            }
+        }else{
+            for (int i=(tFila/2); i<=tFila; i++){
+                if (this.vector[i]==dato){
+                    System.out.println("La posicion es: " +i);
+                }
+            }
+        }
+    }
 }
+
+
+    
