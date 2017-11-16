52f9f02c0f52b6c32fa13c1565812954d12ccb73
David Melo <30818594+davidmelo26@users.noreply.github.com>
2017-11-16 16:57:40 -0500

    Subiendo actividad pila

--- src/estructurasdedatos/cap2/listas/Pila.java
+++ src/estructurasdedatos/cap2/listas/Pila.java
@@ -5,10 +5,74 @@
  */
 package estructurasdedatos.cap2.listas;
 
+import estructurasdedatos.cap1.tiposdedatos.Persona;
+
 /**
  *
  * @author tusk
  */
 public class Pila {
     
+    public Persona dato;
+    public NodoPilaTop top;
+
+    
+    public Persona getDato() {
+        return dato;
+    }
+    public void setDato(Persona dato){
+        this.dato = dato;
+    }   
+    
+    //constructor
+    public Pila() {
+        dato = null;
+    }
+    public boolean esVacia() {
+        return top == null;
+    }
+    
+    public void apilarDatos(String nombre, String cedula, String direccion, String telefono) {
+
+        NodoPilaTop nuevo = new NodoPilaTop();
+        Persona usuario = new Persona();
+        
+        usuario.setNombre(nombre);
+        usuario.setCedula(cedula);
+        usuario.setDireccion(direccion);
+        usuario.setTelefono(telefono);
+        
+        nuevo.setDato(usuario);
+        
+        if (esVacia()){
+            top = nuevo;
+        }else{
+            nuevo.setEnlace(top);
+            top = nuevo;
+        }
+    }
+    
+    public void desapilarDatos(){
+        if (esVacia()) {
+            System.out.println("Pila sin datos.");
+        } else {
+            System.out.println("Eliminacion realizada de: \n\n"
+                    + top.getDato().getNombre() + "\n"
+                    + top.getDato().getCedula() + "\n"
+                    + top.getDato().getDireccion() + "\n"
+                    + top.getDato().getTelefono() + "\n");
+
+            top = top.enlace;
+        }
+    } 
+    
+    public void listar(){
+        if (!(top==null)){
+            NodoPilaTop auxiliar = top;
+            while (auxiliar != null) {
+                System.out.println("Datos persona: " + "" + auxiliar.getDato().getNombre() + " ");
+                auxiliar = auxiliar.getEnlace();
+            }
+        }
+    }
 }
