2914ade1b8659561c793ac14bd8d05319139418a
David Melo <30818594+davidmelo26@users.noreply.github.com>
2017-11-23 23:03:56 -0500

    Actualizar ArbolAVL insertarAVL

--- src/estructurasdedatos/cap4/arboles/ArbolAVL.java
+++ src/estructurasdedatos/cap4/arboles/ArbolAVL.java
@@ -53,6 +53,49 @@ public class ArbolAVL {
             raiz = insertarAVL(nuevo, raiz);
         }
     }
+        public NodoAVL insertarAVL(NodoAVL primer, NodoAVL secund){
+        NodoAVL  nuevoPadre = secund;
+        
+        if(primer.dato < secund.dato){
+            if(secund.hijoIzquierdo == null){
+                secund.hijoIzquierdo = primer;
+            }else{
+                secund.hijoIzquierdo = insertarAVL(primer, secund.hijoIzquierdo);
+                
+                if((factorEquilibrio(secund.hijoIzquierdo) - factorEquilibrio(secund.hijoDerecho) == 2)){
+                    if(primer.dato < secund.hijoIzquierdo.dato){
+                        nuevoPadre = rotarIzquierda(secund);
+                    }
+                }
+            }
+        }else
+            if(primer.dato > secund.dato){
+                if(secund.hijoDerecho == null){
+                    secund.hijoDerecho = primer;
+                }else{
+                    secund.hijoDerecho = insertarAVL(primer, secund.hijoDerecho);
+                    if((factorEquilibrio(secund.hijoDerecho) - factorEquilibrio(secund.hijoIzquierdo) == 2)){
+                        if(primer.dato > secund.hijoDerecho.dato){
+                            nuevoPadre = rotarDerecha(secund);
+                        }
+                    }
+                }
+            }else {
+                System.out.println("Dato ingresado no puede ser igual que ya esta ingresado");
+            }
+            //Factor de equilibrio se actualiza
+            if((secund.hijoIzquierdo == null) && (secund.hijoDerecho != null)){
+                secund.factorEquilibrio = secund.hijoDerecho.factorEquilibrio +1;
+            }else
+                if((secund.hijoDerecho == null) && (secund.hijoIzquierdo != null)){
+                secund.factorEquilibrio = secund.hijoIzquierdo.factorEquilibrio +1;
+            }else {
+                secund.factorEquilibrio = 
+                        Math.max(factorEquilibrio(secund.hijoIzquierdo),factorEquilibrio(secund.hijoIzquierdo)) +1;
+            }
+            return nuevoPadre;
+        }
+    
     //Mostrar
     public void preOrden (NodoArbolBinario raiz){
         if(raiz != null){
